5ed29f9bc71c09f3813d1ac9b114507a53d2ea6f
aldenroberts <aldenroberts@aldenroberts-macbookpro.roam.corp.google.com>
2014-12-18 22:09:45 -0800

    scheduled notification should now reschedule themselves after reboot

--- app/src/main/java/com/example/aldenroberts/testproject/NotificationReceiver.java
+++ app/src/main/java/com/example/aldenroberts/testproject/NotificationReceiver.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -17,19 +18,26 @@ public class NotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("TAG", "Received Notification");
 
-        // Get today:
-        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        if (intent != null && intent.getAction() != null && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Log.d("TAG", "Boot Finished Scheduling Notification");
 
-        // If its Sunday-Thursday:
-        if (calendar.get(java.util.Calendar.DAY_OF_WEEK) <= java.util.Calendar.FRIDAY) {
-            CalendarNotificationManager manager = CalendarNotificationManager.getInstance();
-            Notification notification = manager.buildNotification(context);
+            SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+            CalendarNotificationManager.getInstance().scheduleNotificationAtHour(sharedPref.getInt(context.getString(R.string.reminder_time_pref), 18), context);
+        } else {
+            Log.d("TAG", "Received Notification");
 
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(1, notification);
+            // Get today:
+            java.util.Calendar calendar = java.util.Calendar.getInstance();
+
+            // If its Sunday-Thursday:
+            if (calendar.get(java.util.Calendar.DAY_OF_WEEK) <= java.util.Calendar.FRIDAY) {
+                CalendarNotificationManager manager = CalendarNotificationManager.getInstance();
+                Notification notification = manager.buildNotification(context);
+
+                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.notify(1, notification);
+            }
         }
-
     }
 }
